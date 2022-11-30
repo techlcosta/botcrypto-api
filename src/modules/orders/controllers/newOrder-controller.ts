63dc6a1bee7fd71fae59a87f22824fbae8bb8f07
Leandro Costa <techlcosta@MacBook-Pro-de-Leandro.local>
2022-12-01 07:40:32 +0900

    create new order controller

---
+++ src/modules/orders/controllers/newOrder-controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express'
+import { OrdersRepository } from './../repositories/orders-repository'
+import { NewOrderUseCase } from './../use-cases/newOrder-useCase'
+
+export class NewOrderController {
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { side, symbol, quantity, limitPrice, type, options, automationId, isMaker } = await request.body
+    const { id: userId } = request.user
+
+    const ordersRepository = new OrdersRepository()
+
+    const newOrderUseCase = new NewOrderUseCase(ordersRepository)
+
+    await newOrderUseCase.execute({
+      side,
+      symbol,
+      quantity,
+      limitPrice,
+      type,
+      options,
+      automationId,
+      isMaker,
+      userId
+    })
+
+    return response.status(201).send()
+  }
+}
